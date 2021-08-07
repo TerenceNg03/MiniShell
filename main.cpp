6db316b637406dc3f33f51b1a38a90822442445e
Adam Wu <stoicism03@gmail.com>
2021-08-07 23:27:41 +0800

    Init commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  myshell
+//
+//  Created by Adam Wu on 2021/8/7.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
